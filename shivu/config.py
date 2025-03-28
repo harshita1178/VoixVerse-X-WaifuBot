8fa3b58183ef3caf27c6275460f909a001f4d9f4
harshita1178 <usekarlomujhe@gmail.com>
2025-03-28 17:21:57 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -2,20 +2,20 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "6675050163"
-    sudo_users = "6675050163", "6087651372","7640076990","1420481421","6645960688"
+    OWNER_ID = "8156600797"
+    sudo_users = "7640076990", "7756901810", "8156600797"
     GROUP_ID = -1002499806698
-    TOKEN = "8175826981:AAGrz7p7NiE6t1vDIKDBTdbhHrcjB7MGDAo"
-    mongo_url = "mongodb+srv://sunnymuduli584:<db_password>@sunny.s9g6h.mongodb.net/"
+    TOKEN = "8136650512:AAFC8c8225hPO_q2AFKtZtADMjCRryF1ZJU"
+    mongo_url = "mongodb+srv://Naruto777:hinata654@naruto77.e14xiqc.mongodb.net/?retryWrites=true&w=majority&appName=Naruto77"
     PHOTO_URL = ["https://graph.org/file/09e83a1d89aceabd480c5-2afc46a31083fe23f2.jpg", "https://graph.org/file/0aa659508c1add9ae4c86-2b335aa5262b7b64d2.jpg"]
     SUPPORT_CHAT = "Anime_Circle_Club"
     UPDATE_CHAT = "Waifu_Chan_Bot_updates"
     BOT_USERNAME = "@Waifu_Chan_Robot"
-    CHARA_CHANNEL_ID = "-1002640379822"
-    api_id = 28480539
-    api_hash = "6320d9f1bc1f0b72ad66ebdb9d6bfc2c"
+    CHARA_CHANNEL_ID = "-1002646820042"
+    api_id = 28159105
+    api_hash = "a0936ddf210a7e091e19947c7dc70c91"
 
-    
+
 class Production(Config):
     LOGGER = True
 
