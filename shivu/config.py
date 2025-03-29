db4b3d8c6ca1978e1b6518dfdd5878353de08ef1
harshita1178 <usekarlomujhe@gmail.com>
2025-03-29 10:48:26 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -1,24 +1,19 @@
-class Config(object):
-    LOGGER = True
+class Config(object): LOGGER = True
 
-    # Get this value from my.telegram.org/apps
-    OWNER_ID = "7885908019"
-    sudo_users = "7885908019", "7640076990", "7756901810" 
-    GROUP_ID = -1002640379822
-    TOKEN = "8136650512:AAFC8c8225hPO_q2AFKtZtADMjCRryF1ZJU"  # Updated Token
-    mongo_url = "mongodb+srv://freefire106bc:namita77@harshkichut.g2lvwxn.mongodb.net/?retryWrites=true&w=majority&appName=harshkichut"
-    PHOTO_URL = "https://files.catbox.moe/0nb1p7.jpg" 
-    SUPPORT_CHAT = "blade_x_support"
-    UPDATE_CHAT = "blade_x_community"
-    BOT_USERNAME = "Devine_wifu_bot"
-    CHARA_CHANNEL_ID = "-1002610122908"  # Updated Character Channel
-    api_id = 26626068
-    api_hash = "bf423698bcbe33cfd58b11c78c42caa2"
+# Get this value from my.telegram.org/apps
+OWNER_ID = "7885908019"
+sudo_users = "7885908019", "7640076990", "7756901810" 
+GROUP_ID = -1002640379822
+TOKEN = "8136650512:AAFC8c8225hPO_q2AFKtZtADMjCRryF1ZJU"  # Updated Token
+mongo_url = "mongodb+srv://narutohinata760:madara11798@naruto118.nrpssfq.mongodb.net/?retryWrites=true&w=majority&appName=naruto118"
+PHOTO_URL = "https://files.catbox.moe/0nb1p7.jpg" 
+SUPPORT_CHAT = "blade_x_support"
+UPDATE_CHAT = "blade_x_community"
+BOT_USERNAME = "Devine_wifu_bot"
+CHARA_CHANNEL_ID = "-1002610122908"  # Updated Character Channel
+api_id = 26626068
+api_hash = "bf423698bcbe33cfd58b11c78c42caa2"
 
-    
-class Production(Config):
-    LOGGER = True
+class Production(Config): LOGGER = True
 
-
-class Development(Config):
-    LOGGER = True
+class Development(Config): LOGGER = True
