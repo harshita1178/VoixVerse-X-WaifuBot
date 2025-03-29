6818c8ba831f3c3a1eccbcfbb749b20af09217fa
harshita1178 <usekarlomujhe@gmail.com>
2025-03-29 10:08:59 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -1,28 +1,24 @@
-class Config(object): LOGGER = True
+class Config(object):
+    LOGGER = True
 
-# Bot Owner & Sudo Users
-OWNER_ID = 8156600797
-sudo_users = [7640076990, 7756901810, 8156600797]
+    # Get this value from my.telegram.org/apps
+    OWNER_ID = "7885908019"
+    sudo_users = "7885908019", "7640076990", "7756901810" 
+    GROUP_ID = -1002640379822
+    TOKEN = "8136650512:AAFC8c8225hPO_q2AFKtZtADMjCRryF1ZJU"  # Updated Token
+    mongo_url = "mongodb+srv://freefire106bc:namita77@harshkichut.g2lvwxn.mongodb.net/?retryWrites=true&w=majority&appName=harshkichut"
+    PHOTO_URL = "https://files.catbox.moe/0nb1p7.jpg" 
+    SUPPORT_CHAT = "blade_x_support"
+    UPDATE_CHAT = "blade_x_community"
+    BOT_USERNAME = "Devine_wifu_bot"
+    CHARA_CHANNEL_ID = "-1002610122908"  # Updated Character Channel
+    api_id = 26626068
+    api_hash = "bf423698bcbe33cfd58b11c78c42caa2"
 
-# Bot Credentials
-GROUP_ID = -1002499806698
-TOKEN = "8136650512:AAFC8c8225hPO_q2AFKtZtADMjCRryF1ZJU"
-mongo_url = "mongodb+srv://freefire106bc:harshkichut@harshkichut.g2lvwxn.mongodb.net/?retryWrites=true&w=majority&appName=harshkichut"
+    
+class Production(Config):
+    LOGGER = True
 
-# Bot Settings
-PHOTO_URL = [
-    "https://graph.org/file/09e83a1d89aceabd480c5-2afc46a31083fe23f2.jpg",
-    "https://graph.org/file/0aa659508c1add9ae4c86-2b335aa5262b7b64d2.jpg"
-]
-SUPPORT_CHAT = "Anime_Circle_Club"
-UPDATE_CHAT = "Waifu_Chan_Bot_updates"
-BOT_USERNAME = "@Waifu_Chan_Robot"
-CHARA_CHANNEL_ID = -1002646820042  # Channel ID should be an integer
 
-# API Credentials
-api_id = 28159105
-api_hash = "a0936ddf210a7e091e19947c7dc70c91"
-
-class Production(Config): LOGGER = True
-
-class Development(Config): LOGGER = True
+class Development(Config):
+    LOGGER = True
