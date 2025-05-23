aa2e49ff155ea013602a146db27f94c809eeeb83
harshita1178 <usekarlomujhe@gmail.com>
2025-05-23 11:27:38 +0530

    Update All.py

--- shivu/modules/All.py
+++ shivu/modules/All.py
@@ -4,7 +4,7 @@ from pyrogram.types import Message
 OWNER_ID = 6675050163
 
 @Client.on_message(filters.command("all") & filters.group)
-async def tag_all(c: Client, m: Message):
+async def tag_all_one_by_one(c: Client, m: Message):
     if m.from_user.id != OWNER_ID:
         await m.reply_text("SHUT UP YOU STUPID BAKA 😠 THIS COMMAND IS NOT FOR YOU")
         return
@@ -13,13 +13,14 @@ async def tag_all(c: Client, m: Message):
     if len(text) == 1:
         return await m.reply("Please provide a message after /all")
 
-    msg = text[1]
-    members = []
-    async for member in c.get_chat_members(m.chat.id):
-        if member.user.is_bot:
-            continue
-        members.append(member.user.mention)
+    base_msg = text[1]
 
-    mention_text = " ".join(members)
-    await m.reply(f"**{msg}**\n\n{mention_text}")
-  
+    async for member in c.get_chat_members(m.chat.id):
+        user = member.user
+        if user.is_bot:
+            continue
+        try:
+            await m.reply(f"{base_msg} {user.mention}")
+        except Exception:
+            continue
+            
