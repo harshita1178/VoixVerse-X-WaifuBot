3e590d1145ba224305b254ac793f5e892c3acfa8
harshita1178 <usekarlomujhe@gmail.com>
2025-05-23 11:16:07 +0530

    Update All.py

--- shivu/modules/All.py
+++ shivu/modules/All.py
@@ -1,1 +1,25 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
 
+OWNER_ID = 6675050163
+
+@Client.on_message(filters.command("all") & filters.group)
+async def tag_all(c: Client, m: Message):
+    if m.from_user.id != OWNER_ID:
+        await m.reply_text("SHUT UP YOU STUPID BAKA 😠 THIS COMMAND IS NOT FOR YOU")
+        return
+
+    text = m.text.split(None, 1)
+    if len(text) == 1:
+        return await m.reply("Please provide a message after /all")
+
+    msg = text[1]
+    members = []
+    async for member in c.get_chat_members(m.chat.id):
+        if member.user.is_bot:
+            continue
+        members.append(member.user.mention)
+
+    mention_text = " ".join(members)
+    await m.reply(f"**{msg}**\n\n{mention_text}")
+  
